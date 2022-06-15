61e727a6b8e9654e3856ba5cf2bc674cfc1fb1bb
taduna.kitamura <taduna.kitamura0324@gmail.com>
2022-06-16 01:34:49 +0900

    DestroyServlet.javaを作成

--- src/main/java/filters/EncodingFilter.java
+++ src/main/java/filters/EncodingFilter.java
@@ -9,45 +9,42 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpFilter;
 
 /**
  * Servlet Filter implementation class EncodingFilter
  */
 @WebFilter("/*")
-public class EncodingFilter extends HttpFilter implements Filter {
+public class EncodingFilter implements Filter {
 
     /**
-     * @see HttpFilter#HttpFilter()
+     * Default constructor.
      */
     public EncodingFilter() {
-        super();
-        // TODO Auto-generated constructor stub
+
     }
 
-	/**
-	 * @see Filter#destroy()
-	 */
-	public void destroy() {
-		// TODO Auto-generated method stub
-	}
+    /**
+     * @see Filter#destroy()
+     */
+    public void destroy() {
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		// place your code here
+    }
 
-		// pass the request along the filter chain
-		chain.doFilter(request, response);
-	}
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
 
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
-	public void init(FilterConfig fConfig) throws ServletException {
-		// TODO Auto-generated method stub
-	}
+        chain.doFilter(request, response);
+    }
+
+    /**
+     * @see Filter#init(FilterConfig)
+     */
+    public void init(FilterConfig fConfig) throws ServletException {
+
+    }
 
 }
